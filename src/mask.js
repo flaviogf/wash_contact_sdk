ec902c8e6e425029551096f2fad584b0675082d4
flaviogf <flavio.fernandes6@gmail.com>
2019-08-30 22:28:25 -0300

    Add phone mask

--- src/mask.js
+++ src/mask.js
@@ -1,8 +1,17 @@
 export class Mask {
   static cep(value) {
     return value
-      .replace(/\D/, '')
+      .replace(/\D/g, '')
       .replace(/(\d{5})(\d)/, '$1-$2')
       .replace(/(-\d{3})(\d+$)/, '$1')
+  }
+
+  static phone(value) {
+    return value
+      .replace(/\D/g, '')
+      .replace(/(\d)/, '($1')
+      .replace(/(\d{2})/, '$1) ')
+      .replace(/(\d{4})(\d)/, '$1-$2')
+      .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
   }
 }
--- tests/mask.spec.js
+++ tests/mask.spec.js
@@ -26,4 +26,46 @@ describe('Mask', () => {
       expect(result).to.be.equal('14404-000')
     })
   })
+
+  describe('phone', () => {
+    it('should has phone method', () => {
+      expect(Mask.phone).to.be.a('function')
+    })
+
+    it('should add "(" when 1 digit is passed', () => {
+      const result = Mask.phone('1')
+
+      expect(result).to.be.equal('(1')
+    })
+
+    it('should add ")" when 2 digit is passed', () => {
+      const result = Mask.phone('11')
+
+      expect(result).to.be.equal('(11) ')
+    })
+
+    it('should add "-" when 7 digits is passed', () => {
+      const result = Mask.phone('1199999')
+
+      expect(result).to.be.equal('(11) 9999-9')
+    })
+
+    it('should add "-" when 8 digits is passed', () => {
+      const result = Mask.phone('11999999')
+
+      expect(result).to.be.equal('(11) 9999-99')
+    })
+
+    it('should apply completed mask when a phone with 11 digits is passed', () => {
+      const result = Mask.phone('11999998888')
+
+      expect(result).to.be.equal('(11) 99999-8888')
+    })
+
+    it('should apply completed mask when a phone with 10 digits is passed', () => {
+      const result = Mask.phone('1199998888')
+
+      expect(result).to.be.equal('(11) 9999-8888')
+    })
+  })
 })
